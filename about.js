7d6fe14cb5493b9cd67e4d6c43a1bae1bffabff0
Authoi234 <jawadkhan.authoi@gmail.com>
2022-10-13 13:32:43 +0600

    about is finished

---
+++ about.js
@@ -0,0 +1,17 @@
+const postImg = document.getElementById('post-img');
+postImg.style.width = '4%';
+postImg.style.height = '100%';
+
+
+const inputField = document.getElementById('input-field');
+inputField.style.width = '70%';
+
+document.getElementById('post-img').addEventListener('click', function(){
+    const commentBox = document.getElementById('input-field');
+    const comment = commentBox.value;
+    const commentContainer = document.getElementById('comment-content');
+    const p = document.createElement('p');
+    p.innerText = comment;
+    commentContainer.appendChild(p);
+    commentBox.value = '';
+})
